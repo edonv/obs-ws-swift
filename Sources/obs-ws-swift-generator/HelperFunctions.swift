6a768f6017a0436333f673aacbe224fc52407536
Edon <edon@valdman.works>
2026-05-19 15:13:12 -0500

    Create HelperFunctions.swift
    Added some helper functions.

---
+++ Sources/obs-ws-swift-generator/HelperFunctions.swift
@@ -0,0 +1,46 @@
+//
+//  HelperFunctions.swift
+//  obs-ws-swift
+//
+//  Created by Edon Valdman on 5/18/26.
+//
+
+import Foundation
+
+func camelize(_ string: String) -> String {
+    guard !string.isEmpty else { return "" }
+    var tempStr = string
+    
+    if tempStr.contains("_")
+        && tempStr.allSatisfy({ $0.isUppercase }) {
+        tempStr = tempStr
+            .split(separator: "_")
+            .map { $0.prefix(1) + $0.dropFirst().lowercased() }
+            .joined()
+    }
+    
+    // Catch single words that are all caps
+    if tempStr.allSatisfy({ $0.isUppercase }) {
+        return tempStr.lowercased()
+    }
+    
+    return tempStr.prefix(1).lowercased() + tempStr.dropFirst()
+}
+
+func splitByCapitals(_ string: String) -> [String] {
+    string.enumerated().split { (i, char) in
+        char.isUppercase || i == 0
+    }
+    .map { $0.map(\.element) }
+    .map { String($0) }
+}
+
+func processDescription(_ description: String) -> String {
+    #warning("TODO: Refactor first line to work like original code `findReplaceLinkedSymbolsInDescs`")
+    description
+        .replacingOccurrences(
+            of: "Note:",
+            with: "- Note:",
+            options: .regularExpression
+        )
+}
