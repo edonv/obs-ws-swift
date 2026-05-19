9a336fca9e204fbcd80bcaf18bd97ecc71df008f
Edon <edon@valdman.works>
2026-05-19 15:13:36 -0500

    Create Protocol+Generatable.swift
    Added `Generatable` conformance to `OBSWSProtocol`.

---
+++ Sources/obs-ws-swift-generator/Generatable/Protocol+Generatable.swift
@@ -0,0 +1,31 @@
+//
+//  Protocol+Generatable.swift
+//  obs-ws-swift
+//
+//  Created by Edon Valdman on 5/18/26.
+//
+
+import Foundation
+import SwiftSyntax
+import SwiftSyntaxBuilder
+import SwiftBasicFormat
+
+extension OBSWSProtocol: Generatable {
+    private var format: BasicFormat {
+        .init(indentationWidth: .spaces(2))
+    }
+    
+    func generate() throws -> DeclSyntax {
+        try EnumDeclSyntax("public enum OBSWS") {
+            try EnumDeclSyntax("public enum Enums") {
+                for enumDef in self.enums {
+                    try enumDef.generate()
+                        .with(\.leadingTrivia, .newline)
+                }
+            }
+            .with(\.leadingTrivia, .newline)
+            .with(\.trailingTrivia, .newline)
+        }
+        .formatted(using: format).as(DeclSyntax.self)!
+    }
+}
