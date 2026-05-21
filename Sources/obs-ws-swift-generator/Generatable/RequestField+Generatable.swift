ed1a57c9de7babad5c2f9456fc48ebd92b37cacf
Edon <edon@valdman.works>
2026-05-21 10:11:59 -0500

    Create RequestField+Generatable.swift
    Added `Generatable` conformance to `OBSWSProtocol.Request.RequestField`.

---
+++ Sources/obs-ws-swift-generator/Generatable/RequestField+Generatable.swift
@@ -0,0 +1,16 @@
+//
+//  RequestField+Generatable.swift
+//  obs-ws-swift
+//
+//  Created by Edon Valdman on 5/21/26.
+//
+
+import Foundation
+import SwiftSyntax
+
+extension OBSWSProtocol.Request.RequestField: Generatable {
+    func generate() throws -> VariableDeclSyntax {
+        try VariableDeclSyntax("public let \(raw: self.valueName): \(raw: self.fieldType())")
+            .with(\.leadingTrivia, self.generateDocsTrivia())
+    }
+}
