e72aecfe33d8cd531a0e829d4125ffd7c93c09be
Edon <edon@valdman.works>
2026-05-21 13:22:03 -0500

    Create Event+Generatable.swift
    Added `Generatable` conformance to `OBSWSProtocol.Event`.

---
+++ Sources/obs-ws-swift-generator/Generatable/Event+Generatable.swift
@@ -0,0 +1,43 @@
+//
+//  Event+Generatable.swift
+//  obs-ws-swift
+//
+//  Created by Edon Valdman on 5/21/26.
+//
+
+import Foundation
+import SwiftSyntax
+import SwiftSyntaxBuilder
+
+extension OBSWSProtocol.Event: Generatable {
+    func generate() throws -> StructDeclSyntax {
+        return try StructDeclSyntax("public struct \(raw: self.eventType): OBSEvent") {
+            // - Docs (added at the end outside the builder)
+            
+            // - Properties/Fields
+            for reqField in self.dataFields {
+                try reqField.generate()
+                    .prepending(.newline, to: \.leadingTrivia)
+                    .with(\.trailingTrivia, .newline)
+            }
+            
+            // - Public init
+            let initParamList = FunctionParameterListSyntax {
+                for (i, reqField) in dataFields.enumerated() {
+                    let addComma = i < dataFields.count - 1
+                    FunctionParameterSyntax("\(raw: reqField.valueName): \(raw: reqField.fieldType())\(raw: addComma ? ", ": "")")
+                }
+            }
+            
+            try InitializerDeclSyntax("public init(\(initParamList))") {
+                for reqField in dataFields {
+                    CodeBlockItemSyntax("self.\(raw: reqField.valueName) = \(raw: reqField.valueName)")
+                        .with(\.trailingTrivia, .newline)
+                }
+            }
+            .with(\.leadingTrivia, .newline)
+            .with(\.trailingTrivia, .newline)
+        }
+        .with(\.leadingTrivia, self.generateDocsTrivia())
+    }
+}
