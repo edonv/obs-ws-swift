671c0065c622648ec00663d26a45ed7571867b0f
Edon <edon@valdman.works>
2026-05-21 10:31:21 -0500

    Update Protocol+Generatable.swift
    Refactored `OBSWSProtocol.generate()` to return a `SourceFileSyntax` object.

--- Sources/obs-ws-swift-generator/Generatable/Protocol+Generatable.swift
+++ Sources/obs-ws-swift-generator/Generatable/Protocol+Generatable.swift
@@ -19,18 +19,21 @@ extension OBSWSProtocol: Generatable {
     // For the 1st decl, leading newline moves it to its own line without an extra empty line before
     // For the 2nd decl, it adds to the previous's decl trailing newline, giving an extra empty line between
     
-    func generate() throws -> DeclSyntax {
-        try EnumDeclSyntax("public enum OBSWS") {
-            try EnumDeclSyntax("public enum Enums") {
-                for enumDef in self.enums {
-                    try enumDef.generate()
-                        .with(\.leadingTrivia, .newline)
-                        .with(\.trailingTrivia, .newline)
+    func generate() throws -> SourceFileSyntax {
+        try SourceFileSyntax {
+            try EnumDeclSyntax("public enum OBSWS") {
+                try EnumDeclSyntax("public enum Enums") {
+                    for enumDef in self.enums {
+                        try enumDef.generate()
+                            .with(\.leadingTrivia, .newline)
+                            .with(\.trailingTrivia, .newline)
+                    }
                 }
+                .with(\.leadingTrivia, .newline)
+                .with(\.trailingTrivia, .newline)
             }
-            .with(\.leadingTrivia, .newline)
-            .with(\.trailingTrivia, .newline)
         }
-        .formatted(using: format).as(DeclSyntax.self)!
+        .formatted(using: format)
+        .as(SourceFileSyntax.self)!
     }
 }
