94ef11e56fe5588a8e976448a55caa8d2cbf30dd
Edon <edon@valdman.works>
2026-05-24 09:02:10 -0500

    Update Enum+Generatable.swift
    Added `OptionSet`, `Sendable`, `Hashable`, and `Codable` conformances to `struct` outputs of enums.

--- Sources/obs-ws-swift-generator/Generatable/Enum+Generatable.swift
+++ Sources/obs-ws-swift-generator/Generatable/Enum+Generatable.swift
@@ -32,7 +32,7 @@ extension OBSWSProtocol.Enum: Generatable {
         if isOptionSet {
             #warning("TODO: confirm if `raw` or `literal` is the right way to interpolate a string")
             typeDef = try DeclSyntax(
-                StructDeclSyntax("public struct \(raw: enumName)") {
+                StructDeclSyntax("public struct \(raw: enumName): OptionSet, Sendable, Hashable, Codable") {
                     try VariableDeclSyntax("public let rawValue: Int")
                         .with(\.leadingTrivia, .newline)
                         .with(\.trailingTrivia, .newline)
