d48eef7db9d8192d7c5fc565ea58f5a20b925a28
Edon <edon@valdman.works>
2026-05-24 13:04:19 -0500

    Create RequestBatchResponse.swift
    Added `OBSOpData.RequestBatchResponse`.

---
+++ Sources/OBSWebSocket/Messages/Operations/RequestBatchResponse.swift
@@ -0,0 +1,42 @@
+//
+//  RequestBatchResponse.swift
+//  obs-ws-swift
+//
+//  Created by Edon Valdman on 5/24/26.
+//
+
+import Foundation
+import JSONValue
+
+extension OBSOpData {
+    /// `obs-websocket` is responding to a request batch coming from the client.
+    ///
+    /// - term Sent From: `obs-websocket`
+    /// - term Sent To: Identified client which made the request
+    public struct RequestBatchResponse: OBSOpDataProtocol {
+        public static let opCode: OBSWS.Enums.OpCode = .requestBatchResponse
+        
+        public let id: String
+        public let results: [Response]
+        
+        private enum CodingKeys: String, CodingKey {
+            case id = "requestId"
+            case results
+        }
+        
+        /// Identical to ``OBSOpData/RequestResponse``, except ``id`` is optional.
+        public struct Response: Sendable, Hashable, Codable {
+            public let type: String
+            public let id: String?
+            public let status: RequestResponse.Status
+            public let data: JSONValue?
+            
+            private enum CodingKeys: String, CodingKey {
+                case type = "requestType"
+                case id = "requestId"
+                case status = "requestStatus"
+                case data = "responseData"
+            }
+        }
+    }
+}
