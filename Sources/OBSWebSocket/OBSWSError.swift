2b2c0f005a32e02cb2d67ef70624119066fdb959
Edon <edon@valdman.works>
2026-05-24 12:11:55 -0500

    Create OBSWSError.swift
    Added new `OBSWS.Error` error type.

---
+++ Sources/OBSWebSocket/OBSWSError.swift
@@ -0,0 +1,21 @@
+//
+//  OBSWSError.swift
+//  obs-ws-swift
+//
+//  Created by Edon Valdman on 5/24/26.
+//
+
+import Foundation
+
+extension OBSWS {
+    public enum Error: Swift.Error {
+        case missingPasswordWhereRequired
+        
+        public var localizedDescription: String {
+            switch self {
+            case .missingPasswordWhereRequired:
+                return "OBS requires a password and one wasn't given."
+            }
+        }
+    }
+}
