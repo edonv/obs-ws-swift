3df466891228308e97e619bd02866efcfb21ff90
Edon <edon@valdman.works>
2026-05-24 11:51:45 -0500

    Create OBSMessage.swift
    Added `OBSMessage` type.

---
+++ Sources/OBSWebSocket/Messages/OBSMessage.swift
@@ -0,0 +1,27 @@
+//
+//  OBSMessage.swift
+//  obs-ws-swift
+//
+//  Created by Edon Valdman on 5/24/26.
+//
+
+import Foundation
+
+/// A type used for sending and receiving information to and from OBS.
+public struct OBSMessage<Body: OBSOpDataProtocol>: OBSMessageProtocol {
+    public let operation: OBSWS.Enums.OpCode
+    public let data: Body
+    
+    public init(
+        operation: OBSWS.Enums.OpCode,
+        data: Body
+    ) {
+        self.operation = operation
+        self.data = data
+    }
+    
+    public init(data: Body) {
+        self.operation = Body.opCode
+        self.data = data
+    }
+}
