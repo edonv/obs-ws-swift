1800a637b5e27345d56fe314823d5edd43cef43f
Edon <edon@valdman.works>
2026-05-24 12:19:25 -0500

    Create Reidentify.swift
    Added `OBSOpData.Reidentify`.

---
+++ Sources/OBSWebSocket/Messages/Operations/Reidentify.swift
@@ -0,0 +1,22 @@
+//
+//  Reidentify.swift
+//  obs-ws-swift
+//
+//  Created by Edon Valdman on 5/24/26.
+//
+
+import Foundation
+
+extension OBSOpData {
+    /// Sent at any time after initial identification to update the provided session parameters.
+    ///
+    /// Only the listed parameters may be changed after initial identification. To change
+    /// a parameter not listed, you must reconnect to the `obs-websocket` server.
+    /// - term Sent From: Identified client
+    /// - term Sent To: `obs-websocket`
+    public struct Reidentify: OBSOpDataProtocol {
+        public static let opCode: OBSWS.Enums.OpCode = .reidentify
+        
+        public let eventSubscriptions: OBSWS.Enums.EventSubscription?
+    }
+}
