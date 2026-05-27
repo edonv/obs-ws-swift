1eb33505bc757bc9b061ef5230bf5503c8b3c40b
Edon <edon@valdman.works>
2026-05-26 21:49:04 -0500

    Create OBSWebSocketCoders.swift
    Created internal protocols (and conformances) to give JSON coders and MessagePack coders shared ground.

---
+++ Sources/OBSWebSocket/Misc/OBSWebSocketCoders.swift
@@ -0,0 +1,36 @@
+//
+//  OBSWebSocketCoders.swift
+//  obs-ws-swift
+//
+//  Created by Edon Valdman on 5/26/26.
+//
+
+import Foundation
+import MessagePacker
+
+// MARK: - Protocols
+
+protocol OBSWebSocketEncoder: Sendable, Copyable {
+    init()
+    
+    func encode<T: Encodable>(_ value: T) throws -> Data
+}
+
+protocol OBSWebSocketDecoder: Sendable, Copyable {
+    init()
+    
+    func decode<T:Decodable>(
+        _ type: T.Type,
+        from data: Data
+    ) throws -> T
+}
+
+// MARK: - JSON
+
+extension JSONEncoder: OBSWebSocketEncoder {}
+extension JSONDecoder: OBSWebSocketDecoder {}
+
+// MARK: - MessagePack
+
+extension MessagePackEncoder: OBSWebSocketEncoder {}
+extension MessagePackDecoder: OBSWebSocketDecoder {}
