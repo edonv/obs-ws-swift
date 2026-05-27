71e294b57c0ab28ebbb2a60bc6a05de8c4de1da0
Edon <edon@valdman.works>
2026-05-27 16:02:17 -0500

    Update Package.swift
    Updated minimum-supported OS versions due to use of new `Mutex` type.

--- Package.swift
+++ Package.swift
@@ -6,8 +6,9 @@ import PackageDescription
 let package = Package(
     name: "obs-ws-swift",
     platforms: [
-        .iOS(.v13),
-        .macOS(.v10_15),
+        // for support of `Mutex` type
+        .iOS(.v18),
+        .macOS(.v15),
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
