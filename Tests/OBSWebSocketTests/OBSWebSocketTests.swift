5d856bfbdb894466c4377c3997abbabb40b6a780
Edon <edon@valdman.works>
2026-05-27 16:48:52 -0500

    Update OBSWebSocketTests.swift
    Added basic test function for testing connection stability.

--- Tests/OBSWebSocketTests/OBSWebSocketTests.swift
+++ Tests/OBSWebSocketTests/OBSWebSocketTests.swift
@@ -1,6 +1,28 @@
 import Testing
 @testable import OBSWebSocket
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+struct OBSWebSocketTests {
+    @Test func webSocketConnection() async throws {
+        let obsWS = OBSWebSocket()
+        try await obsWS.connect(
+            with: .init(
+//                scheme: "wss",
+                ipAddress: "192.168.1.178",
+                port: 4455,
+                password: "thisisatest",
+                encodingProtocol: .json
+            ),
+            subscribingTo: .all
+        )
+        
+        #expect(obsWS.activeConnectionDetails != nil)
+        
+        try await Task.sleep(for: .seconds(5))
+        
+        obsWS.disconnect(with: .invalidDataFieldType, reason: "This is a test")
+        
+//        for try await msg in obsWS.messages {
+//            print(msg)
+//        }
+    }
 }
