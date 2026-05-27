b5ed8fe58c29f7fa0133bfbf9b6d8bb32d567073
Edon <edon@valdman.works>
2026-05-27 12:46:42 -0500

    Update Hello.swift
    Refactored `.toIdentify(password:subscribeTo:)` to be a function on `OBSMessage<OBSOpData.Hello>` (`OBSMessages.Hello`).

--- Sources/OBSWebSocket/Messages/Operations/Hello.swift
+++ Sources/OBSWebSocket/Messages/Operations/Hello.swift
@@ -47,49 +47,51 @@ extension OBSOpData {
                 self.salt = salt
             }
         }
+    }
+}
+
+extension OBSMessages.Hello {
+    /// Maps `Hello` instance to a new ``OBSMessages/Identify`` (``OBSMessage`` with an ``OBSOpData/Identify`` `Body`).
+    /// - Parameters:
+    ///   - password: If provided, it's used with ``OBSOpData/Hello/authentication`` to create a final
+    ///   authentication string.
+    ///   - events: If provided, it tells `obs-websocket` that it's interested in being
+    ///   alerted about specific categories of ``OBSWS/Events``.
+    /// - Throws: ``OBSWS/Error/missingPasswordWhereRequired`` if
+    /// ``OBSOpData/Hello/authentication`` is present without a provided password.
+    /// - Returns: A new ``OBSMessages/Identify`` (``OBSMessage`` with an ``OBSOpData/Identify`` `Body`) with the generated authentication string.
+    internal func toIdentify(
+        password: String?,
+        subscribeTo events: OBSWS.Enums.EventSubscription?
+    ) throws -> OBSMessages.Identify {
+        var auth: String? = nil
         
-        /// Maps `Hello` instance to a new ``Identify`` message body.
-        /// - Parameters:
-        ///   - password: If provided, it's used with ``Hello/authentication`` to create a final
-        ///   authentication string.
-        ///   - events: If provided, it tells `obs-websocket` that it's interested in being
-        ///   alerted about specific categories of ``OBSWS/Events``.
-        /// - Throws: ``OBSWS/Error/missingPasswordWhereRequired`` if
-        /// ``Hello/authentication`` is present without a provided password.
-        /// - Returns: A new ``Identify`` message body with the generated authentication string.
-        internal func toIdentify(
-            password: String?,
-            subscribeTo events: OBSWS.Enums.EventSubscription?
-        ) throws -> Identify {
-            var auth: String? = nil
-            
-            // To generate the authentication string, follow these steps:
-            if let a = authentication {
-                if let pass = password,
-                   !pass.isEmpty {
-                    // Concatenate the websocket password with the salt provided by the server (password + salt)
-                    let secretString = pass + a.salt
-                    
-                    // Generate an SHA256 binary hash of the result and base64 encode it, known as a base64 secret.
-                    let secretHash = SHA256.hash(data: secretString.data(using: .utf8)!)
-                    let encodedSecret = Data(secretHash)
-                        .base64EncodedString()
-                    
-                    // Concatenate the base64 secret with the challenge sent by the server (base64_secret + challenge)
-                    let authResponseString = encodedSecret + a.challenge
-                    
-                    // Generate a binary SHA256 hash of that result and base64 encode it. You now have your authentication string.
-                    let authResponseHash = SHA256.hash(data: authResponseString.data(using: .utf8)!)
-                    auth = Data(authResponseHash)
-                        .base64EncodedString()
-                } else {
-                    // If there is authentication data in the Hello message, then it requires a password.
-                    // If the user didn't enter a password where one is required, throw error.
-                    throw OBSWS.Error.missingPasswordWhereRequired
-                }
+        // To generate the authentication string, follow these steps:
+        if let a = self.data.authentication {
+            if let pass = password,
+               !pass.isEmpty {
+                // Concatenate the websocket password with the salt provided by the server (password + salt)
+                let secretString = pass + a.salt
+                
+                // Generate an SHA256 binary hash of the result and base64 encode it, known as a base64 secret.
+                let secretHash = SHA256.hash(data: secretString.data(using: .utf8)!)
+                let encodedSecret = Data(secretHash)
+                    .base64EncodedString()
+                
+                // Concatenate the base64 secret with the challenge sent by the server (base64_secret + challenge)
+                let authResponseString = encodedSecret + a.challenge
+                
+                // Generate a binary SHA256 hash of that result and base64 encode it. You now have your authentication string.
+                let authResponseHash = SHA256.hash(data: authResponseString.data(using: .utf8)!)
+                auth = Data(authResponseHash)
+                    .base64EncodedString()
+            } else {
+                // If there is authentication data in the Hello message, then it requires a password.
+                // If the user didn't enter a password where one is required, throw error.
+                throw OBSWS.Error.missingPasswordWhereRequired
             }
-            
-            return Identify(rpcVersion: rpcVersion, authentication: auth, eventSubscriptions: events)
         }
+        
+        return .init(data: .init(rpcVersion: self.data.rpcVersion, authentication: auth, eventSubscriptions: events))
     }
 }
