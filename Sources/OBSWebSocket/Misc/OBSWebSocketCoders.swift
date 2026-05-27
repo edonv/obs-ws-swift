a58830e0752eb13b00b60d7e99df42e6de6e8ac1
Edon <edon@valdman.works>
2026-05-27 16:09:41 -0500

    Update OBSWebSocketCoders.swift
    Added more to `OBSWebSocketCoders`. Added `AnyOBSWSEncoder`/`AnyOBSWSDecoder`, and some convenience types/functions.

--- Sources/OBSWebSocket/Misc/OBSWebSocketCoders.swift
+++ Sources/OBSWebSocket/Misc/OBSWebSocketCoders.swift
@@ -10,14 +10,26 @@ import MessagePacker
 
 // MARK: - Protocols
 
-protocol OBSWebSocketEncoder: Sendable, Copyable {
-    init()
+protocol OBSWSEncoder: Sendable, Copyable {
+//    init()
     
     func encode<T: Encodable>(_ value: T) throws -> Data
 }
 
-protocol OBSWebSocketDecoder: Sendable, Copyable {
-    init()
+struct AnyOBSWSEncoder: OBSWSEncoder {
+    let encodeCallback: @Sendable (Encodable) throws -> Data
+    
+    init(encode: @Sendable @escaping (Encodable) throws -> Data) {
+        self.encodeCallback = encode
+    }
+    
+    func encode<T>(_ value: T) throws -> Data where T: Encodable {
+        try encodeCallback(value)
+    }
+}
+
+protocol OBSWSDecoder: Sendable, Copyable {
+//    init()
     
     func decode<T: Decodable>(
         _ type: T.Type,
@@ -25,12 +37,72 @@ protocol OBSWebSocketDecoder: Sendable, Copyable {
     ) throws -> T
 }
 
+struct AnyOBSWSDecoder: OBSWSDecoder {
+    let decodeCallback: @Sendable (Decodable.Type, Data) throws -> Decodable
+    
+    init(decode: @Sendable @escaping (Decodable.Type, Data) throws -> Decodable) {
+        self.decodeCallback = decode
+    }
+    
+    func decode<T>(_ type: T.Type, from data: Data) throws -> T where T: Decodable {
+        try decodeCallback(type, data) as! T
+    }
+}
+
+protocol CoderPair {
+    associatedtype Encoder: OBSWSEncoder
+    associatedtype Decoder: OBSWSDecoder
+    
+    var encoder: Encoder { get }
+    var decoder: Decoder { get }
+}
+
+extension CoderPair {
+    func toAny() -> AnyCoderPair {
+        .init(encoder: self.encoder, decoder: self.decoder)
+    }
+}
+
+struct AnyCoderPair: CoderPair {
+    let encoder: AnyOBSWSEncoder
+    let decoder: AnyOBSWSDecoder
+    
+    init<E: OBSWSEncoder, D: OBSWSDecoder>(encoder: E, decoder: D) {
+        self.encoder = .init { encodable in
+            try encoder.encode(encodable)
+        }
+        self.decoder = .init { decodable, data in
+            try decoder.decode(decodable, from: data)
+        }
+    }
+}
+
 // MARK: - JSON
 
-extension JSONEncoder: OBSWebSocketEncoder {}
-extension JSONDecoder: OBSWebSocketDecoder {}
+extension JSONEncoder: OBSWSEncoder {}
+extension JSONDecoder: OBSWSDecoder {}
+
+internal struct JSONCoders: CoderPair {
+    let encoder: JSONEncoder = .init()
+    let decoder: JSONDecoder = .init()
+}
 
 // MARK: - MessagePack
 
-extension MessagePackEncoder: OBSWebSocketEncoder {}
-extension MessagePackDecoder: OBSWebSocketDecoder {}
+extension MessagePackEncoder: OBSWSEncoder {}
+extension MessagePackDecoder: OBSWSDecoder {}
+
+internal struct MessagePackCoders: CoderPair {
+    let encoder: MessagePackEncoder = .init()
+    let decoder: MessagePackDecoder = .init()
+}
+
+extension AnyCoderPair {
+    static func pair(for mode: OBSWebSocket.Connection.MessageEncoding?) -> Self {
+        mode == .msgPack
+        ? .msgPack
+        : .json
+    }
+    static var json: Self { JSONCoders().toAny() }
+    static var msgPack: Self { MessagePackCoders().toAny() }
+}
