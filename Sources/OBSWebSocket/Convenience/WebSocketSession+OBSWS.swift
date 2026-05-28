ecb9df74cbcb706e8d20ec3fc75449ef551ec158
Edon <edon@valdman.works>
2026-05-28 10:32:23 -0500

    Create WebSocketSession+OBSWS.swift
    Should have been committed before 8d36655b102a14db0d2c0d3f49862654677118a3.

---
+++ Sources/OBSWebSocket/Convenience/WebSocketSession+OBSWS.swift
@@ -0,0 +1,66 @@
+//
+//  WebSocketSession+OBSWS.swift
+//  obs-ws-swift
+//
+//  Created by Edon Valdman on 5/27/26.
+//
+
+import Foundation
+import MessagePacker
+
+extension WebSocketSession {
+    internal func send<M: OBSMessageProtocol>(
+        _ message: M,
+        encodingProtocol: OBSWebSocket.ConnectionDetails.MessageEncoding
+    ) async throws {
+        let data: Data
+        
+        switch encodingProtocol {
+        case .json:
+            data = try JSONCoders().encoder.encode(message)
+        case .msgPack:
+            data = try MessagePackCoders().encoder.encode(message)
+        }
+        
+        try await self.send(obsWSData: data, encodingProtocol: encodingProtocol)
+    }
+    
+    private func send(
+        obsWSData data: Data,
+        encodingProtocol: OBSWebSocket.ConnectionDetails.MessageEncoding
+    ) async throws {
+        switch encodingProtocol {
+        case .json:
+            try await self.send(String(data: data, encoding: .utf8)!)
+        case .msgPack:
+            try await self.send(data)
+        }
+    }
+}
+
+extension WebSocketSession.Messages {
+    /// Map each message to an ``OBSUntypedMessage``.
+    func asOBSWSMessages<Decoder: OBSWSDecoder>(_ decoder: Decoder) -> some AsyncSequence<OBSUntypedMessage, any Error> {
+        self.map { msg in
+            let decodable: Data
+            switch msg {
+            case .string(let str):
+                guard let data = str.data(using: .utf8) else {
+                    #warning("TODO: new error for this")
+                    throw OBSWebSocket.Errors.test
+                }
+                
+                decodable = data
+                
+            case .data(let data):
+                decodable = data
+                
+            @unknown default:
+                #warning("TODO: new error for this")
+                throw OBSWebSocket.Errors.test
+            }
+            
+            return try decoder.decode(OBSUntypedMessage.self, from: decodable)
+        }
+    }
+}
