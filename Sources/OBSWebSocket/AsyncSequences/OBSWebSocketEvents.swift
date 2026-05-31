160674478dc4299efe719e7f2b482ca9f0bb8287
Edon <edon@valdman.works>
2026-05-31 11:34:15 -0500

    Create OBSWebSocketEvents.swift
    Created new `AsyncSequence` type for mapping `OBSUntypedMessage`s to `OBSOpData.Event`s.

---
+++ Sources/OBSWebSocket/AsyncSequences/OBSWebSocketEvents.swift
@@ -0,0 +1,47 @@
+//
+//  OBSWebSocketEvents.swift
+//  obs-ws-swift
+//
+//  Created by Edon Valdman on 5/26/26.
+//
+
+import Foundation
+
+extension OBSWebSocket {
+    public struct Events<Base: AsyncSequence>: AsyncSequence, Sendable where Base: Sendable, Base.Element == OBSUntypedMessage {
+        public typealias Element = OBSOpData.Event
+        
+        let base: Base
+        private let isIncluded: @Sendable (Element) throws -> Bool
+
+        internal init(
+            _ base: Base,
+            isIncluded: @escaping @Sendable (Element) throws -> Bool
+        ) {
+            self.base = base
+            self.isIncluded = isIncluded
+        }
+        
+        public func makeAsyncIterator() -> AsyncIterator {
+            AsyncIterator(
+                iterator: base.makeAsyncIterator(),
+                isIncluded: isIncluded
+            )
+        }
+        
+        public struct AsyncIterator: AsyncIteratorProtocol {
+            var iterator: Base.AsyncIterator
+            let isIncluded: @Sendable (Element) throws -> Bool
+            
+            public mutating func next() async throws -> Element? {
+                while let msg = try await iterator.next() {
+                    guard let eventMsg = try? msg.as(OBSOpData.Event.self),
+                          try self.isIncluded(eventMsg.data) else { continue }
+                    return eventMsg.data
+                }
+                
+                return nil
+            }
+        }
+    }
+}
