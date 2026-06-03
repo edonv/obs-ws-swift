86162977846b8b1838037d0a0ffdf84d038c4340
Edon <edon@valdman.works>
2026-06-03 13:34:34 -0500

    Moved message-related protocols into new folder

---
+++ Sources/OBSWebSocket/Protocols/OBSRequestResponse.swift
@@ -0,0 +1,12 @@
+//
+//  OBSRequestResponse.swift
+//  obs-ws-swift
+//
+//  Created by Edon Valdman on 6/3/26.
+//
+
+import Foundation
+
+//public protocol OBSRequestResponse: Sendable, Hashable, Codable {
+//    
+//}
