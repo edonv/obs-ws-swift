a24d51c33c90b51bd8805f7777dcb4a76c3eddea
Edon <edon@valdman.works>
2026-06-03 14:54:02 -0500

    Updated all uses of `OBSMessage` to `OBS.Message`

--- Sources/OBSWebSocket/Messages/OBSMessage.swift
+++ Sources/OBSWebSocket/Messages/OBSMessage.swift
@@ -8,43 +8,45 @@
 import Foundation
 import JSONValue
 
-/// A type used for sending and receiving information to and from OBS.
-public struct OBSMessage<Body: OBSOpDataProtocol>: OBSMessageProtocol {
-    public typealias OpCode = OBSWS.Enums.OpCode
-    
-    public let operation: OpCode
-    public let data: Body
-    
-    public init(
-        operation: OpCode,
-        data: Body
-    ) {
-        self.operation = operation
-        self.data = data
-    }
-    
-    public init(data: Body) {
-        self.operation = Body.opCode
-        self.data = data
-    }
-    
-    func untyped() throws -> OBSUntypedMessage {
-        .init(
-            operation: operation,
-            data: try JSONValue.fromCodable(data)
-        )
+extension OBS {
+    /// A type used for sending and receiving information to and from OBS.
+    public struct Message<Body: OBSOpDataProtocol>: OBSMessageProtocol {
+        public typealias OpCode = OBSWS.Enums.OpCode
+        
+        public let operation: OpCode
+        public let data: Body
+        
+        public init(
+            operation: OpCode,
+            data: Body
+        ) {
+            self.operation = operation
+            self.data = data
+        }
+        
+        public init(data: Body) {
+            self.operation = Body.opCode
+            self.data = data
+        }
+        
+        func untyped() throws -> OBSUntypedMessage {
+            .init(
+                operation: operation,
+                data: try JSONValue.fromCodable(data)
+            )
+        }
     }
 }
 
-/// Namespace for type aliases of typed ``OBSMessage``s.
+/// Namespace for type aliases of typed ``OBS/Message``s.
 public enum OBSMessages {
-    public typealias Hello = OBSMessage<OBS.OpData.Hello>
-    public typealias Identify = OBSMessage<OBS.OpData.Identify>
-    public typealias Identified = OBSMessage<OBS.OpData.Identified>
-    public typealias Reidentify = OBSMessage<OBS.OpData.Reidentify>
-    public typealias Event = OBSMessage<OBS.OpData.Event>
-    public typealias Request = OBSMessage<OBS.OpData.Request>
-    public typealias RequestResponse = OBSMessage<OBS.OpData.RequestResponse>
-    public typealias RequestBatch = OBSMessage<OBS.OpData.RequestBatch>
-    public typealias RequestBatchResponse = OBSMessage<OBS.OpData.RequestBatchResponse>
+    public typealias Hello = OBS.Message<OBS.OpData.Hello>
+    public typealias Identify = OBS.Message<OBS.OpData.Identify>
+    public typealias Identified = OBS.Message<OBS.OpData.Identified>
+    public typealias Reidentify = OBS.Message<OBS.OpData.Reidentify>
+    public typealias Event = OBS.Message<OBS.OpData.Event>
+    public typealias Request = OBS.Message<OBS.OpData.Request>
+    public typealias RequestResponse = OBS.Message<OBS.OpData.RequestResponse>
+    public typealias RequestBatch = OBS.Message<OBS.OpData.RequestBatch>
+    public typealias RequestBatchResponse = OBS.Message<OBS.OpData.RequestBatchResponse>
 }
