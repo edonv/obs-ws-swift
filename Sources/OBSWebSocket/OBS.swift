aa166e66f3e1d6cb963574dc70ba6e5d9fcb03f3
Edon <edon@valdman.works>
2026-06-03 14:41:40 -0500

    Create OBS.swift
    Added `OBS` enum as a top-level namespace.

---
+++ Sources/OBSWebSocket/OBS.swift
@@ -0,0 +1,11 @@
+//
+//  OBS.swift
+//  obs-ws-swift
+//
+//  Created by Edon Valdman on 6/3/26.
+//
+
+import Foundation
+
+/// Top-level namespace for all generated and helper types.
+public enum OBS {}
