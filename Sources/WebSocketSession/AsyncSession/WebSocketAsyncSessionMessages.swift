78bd13b1f927cb073fd781b0ff66c9d276ce14a5
Edon <edon@valdman.works>
2026-06-04 11:03:49 -0500

    Update WebSocketAsyncSessionMessages.swift
    Implemented usage of `WebSocketAsyncSession.debug` `@TaskLocal` property.

--- Sources/WebSocketSession/AsyncSession/WebSocketAsyncSessionMessages.swift
+++ Sources/WebSocketSession/AsyncSession/WebSocketAsyncSessionMessages.swift
@@ -27,17 +27,23 @@ extension WebSocketAsyncSession {
         }
         
         internal func yield(_ element: Element) {
-            print("yield(_:)", element)
+            if WebSocketAsyncSession.debug {
+                print("yield(_:)", element)
+            }
             self.continuation.yield(element)
         }
         
         internal func yield(with result: Result<Element, Swift.Error>) {
-            print("yield(with:)", result)
+            if WebSocketAsyncSession.debug {
+                print("yield(with:)", result)
+            }
             self.continuation.yield(with: result)
         }
         
         internal func finish(throwing error: Error) {
-            print("finish(throwing:)", error)
+            if WebSocketAsyncSession.debug {
+                print("finish(throwing:)", error)
+            }
             self.continuation.finish(throwing: error)
         }
         
